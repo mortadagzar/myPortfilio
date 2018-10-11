716c2fa854ce88a7a93bedeb24f81711dad57cde
mortada gzar <mortadagzar@gmail.com>
2018-10-10 21:53:20 -0700

    uh

---
+++ css/js.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function() {
+   
+    
+
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 5) { // this refers to window
+            alert("You've scrolled 100 pixels.");
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
