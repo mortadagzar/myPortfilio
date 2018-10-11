4b6a32cc4bbbdc7da70d9c7adff075fa8653ad82
mortada gzar <mortadagzar@gmail.com>
2018-10-11 09:12:18 -0700

    bh

--- css/js.js
+++ css/js.js
@@ -1,25 +1,11 @@
-
 $(document).ready(function() {
-   
+  
     
 
 
-    $(window).scroll(function() {
-        if ($(window).scrollTop() >= 100) { // this refers to window
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: $(".fourth").offset().top
-            }, 1000);
-            clearTimeout(timeout);
-
-
-        }
-    });
-
-
-
-
-
+    $(document).scroll(function() {
+        console.log($(document).scrollTop());
+    })
 
 
 
